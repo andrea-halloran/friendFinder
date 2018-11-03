e8ad385f0576d92f66d0f7b9c08d288f7c9fb112
ahalloran <ahalloran01@gmail.com>
2018-11-03 01:14:15 -0500

    minor updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,6 +13,40 @@ var tableArray = [{
         4,
         1
     ]
-}];
+}, 
+{
+    "name": "Taylor",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwja6ZDxx7feAhVNQ6wKHZYfDjMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.npr.org%2F2014%2F10%2F31%2F359827368%2Fanything-that-connects-a-conversation-with-taylor-swift&psig=AOvVaw1Llw1zOq8imgIORoIB8KM5&ust=1541311751635321",
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
+}, 
+{
+    "name": "Bob",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjok4OYyLfeAhVJlKwKHR-fASoQjRx6BAgBEAU&url=https%3A%2F%2Fwww.facebook.com%2FBobtheBuilder%2F&psig=AOvVaw09NOOm4zixk1CnQddK4j36&ust=1541311812832773",
+    "scores": [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+}
+
+];
 
 module.exports = tableArray; 
